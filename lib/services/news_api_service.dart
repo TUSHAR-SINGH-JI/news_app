0d874e323b7e7c226d927a88dc4fdc983e647e74
Tushar Singh <tushar458155@gmail.com>
2025-07-21 09:49:23 +0530

    Setup .env and gitignore for API key privacy

--- lib/services/news_api_service.dart
+++ lib/services/news_api_service.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/news_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import '../services/api_key.env'; // Ensure you have your API key in this file
 class NewsApiService {
-  static const String _apiKey = '5bbaec85a87389f50a4960af9a9dbc98';
+  static String _apiKey = dotenv.env['api_key'] ?? '';
   static const String _baseUrl = 'https://gnews.io/api/v4/';
 
   Future<List<Article>> fetchArticles({String query = 'flutter', String category = ''}) async {
